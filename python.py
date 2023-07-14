049460eea717489a7a5127b2c2459b7c40216e4d
Grigory <lololohka057@gmail.com>
2023-07-14 17:28:45 +0300

    test_2

--- python.py
+++ python.py
@@ -13,8 +13,7 @@ class MyServer(BaseHTTPRequestHandler):
     """
     
     def __clients(self):
-      return """
-<!doctype html>
+      return """<!doctype html>
 <html lang="en" data-bs-theme="auto">
   <head><script src="/docs/5.3/assets/js/color-modes.js"></script>
 
@@ -29,7 +28,6 @@ class MyServer(BaseHTTPRequestHandler):
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
       integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 
-    
 
 <link href="/docs/5.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 
@@ -37,14 +35,13 @@ class MyServer(BaseHTTPRequestHandler):
 <link rel="apple-touch-icon" href="/docs/5.3/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
 <link rel="icon" href="/docs/5.3/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
 <link rel="icon" href="/docs/5.3/assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
-<link rel="manifest" href="/docs/5.3/assets/img/favicons/manifest.json">
 <link rel="mask-icon" href="/docs/5.3/assets/img/favicons/safari-pinned-tab.svg" color="#712cf9">
 <link rel="icon" href="/docs/5.3/assets/img/favicons/favicon.ico">
 <meta name="theme-color" content="#712cf9">
     
     <!-- Custom styles for this template -->
     
-    <link href="css.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/plain" href="https://raw.githubusercontent.com/AraTop/test/main/css.css">
   </head>
   <body>
     <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
@@ -156,11 +153,11 @@ class MyServer(BaseHTTPRequestHandler):
     <form>
       <div>
         <label for="name">Имя:</label>
-        <input name="name" type="text" id="name" required>
+        <input type="text" id="name" name="name" required>
       </div>
       <div>
         <label for="email">Почта:</label>
-        <input name="email" type="email" id="email" required>
+        <input type="email" id="email" name="email" required>
       </div>
       <div>
         <label for="message">Текст:</label>
@@ -176,8 +173,7 @@ class MyServer(BaseHTTPRequestHandler):
 <script src="/docs/5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 
     <script src="sidebars.js"></script></body>
-</html>
-"""
+</html>"""
 
     def do_GET(self):
         """ Метод для обработки входящих GET-запросов """
@@ -185,9 +181,9 @@ class MyServer(BaseHTTPRequestHandler):
         print(query_)
         page_content = self.__clients()
         self.send_response(200)
-        self.send_header("Content-type", "text/html") 
+        self.send_header("Content-type", "text/html")
         self.end_headers() 
-        self.wfile.write(bytes(page_content, "utf-8")) 
+        self.wfile.write(bytes(page_content, "utf-8"))
 
 if __name__ == "__main__":        
     webServer = HTTPServer((hostName, serverPort), MyServer)
